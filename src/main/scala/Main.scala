74ceabc72c7f3660ef528579da050f53638ca2a0
Michel Perez <michel.ingesoft@gmail.com>
2020-05-28 18:06:41 -0500

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,22 @@
 import scala.language.implicitConversions
 
+case class Container(num: Float)
+
+trait Mapper[A, B] {
+  def map(a: A): B
+}
+
 object ImplicitConversions {
+  implicit val containerToInt: Mapper[Container, Int] =
+    new Mapper[Container, Int] {
+      def map(cont: Container) = cont.num.toInt
+    }
+
   implicit def floatToInt(x: Float) =
     x.toInt
+
+  implicit def listAToB[A, B](ls: List[A])(implicit m: Mapper[A, B]): List[B] =
+    ls.map(m.map)
 }
 
 trait Monoid[A] {
@@ -35,5 +49,13 @@ object Main extends App {
   println(f"${sum(1, 1.3f)}")
   println(f"${sum(1, 1)}")
   println(sum(List(1, 2, 3))) // uses intMonoid implicitly
+  println(
+    sum[Int](List(1.1f, 2.0f, 3.0f))
+  ) // uses intMonoid implicitly but first parses implicitly
+
+  println(
+    sum[Int](List[Container](Container(1.1f)))
+  ) // uses intMonoid implicitly but first parses implicitly
+
   println(sum(List("a", "b", "c"))) // uses stringMonoid implicitly
 }
